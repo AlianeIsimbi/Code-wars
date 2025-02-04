d62b09b06cd7c495b29e134e289aabe13abccea0
AlianeIsimbi <isimbialiane@gmail.com>
2025-02-04 17:08:27 +0200

    A function that writes number in an array as a phone number

---
+++ Create-phone-number.js
@@ -0,0 +1,5 @@
+function createPhoneNumber(numbers){
+    for(i = 0; i=10; i++){
+      return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+    }
+  }
