4eb57dcef396a32dd213d685a081566486c8dff8
Aliane ISIMBI <117778528+AlianeIsimbi@users.noreply.github.com>
2025-02-17 10:16:50 +0200

    Add solution - CodeHub

---
+++ 6-kyu/the-vowel-code/the-vowel-code.js
@@ -0,0 +1,7 @@
+function encode(string) {
+  return string.replaceAll('a', 1).replaceAll('e', 2).replaceAll('i', 3).replaceAll('o', 4).replaceAll('u', 5);
+}
+​
+function decode(string) {
+  return string.replaceAll(1, 'a').replaceAll(2, 'e').replaceAll(3, 'i').replaceAll(4, 'o').replaceAll(5, 'u');
+}
