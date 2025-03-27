d1c484a5c962bef740b25e7178ebc72cd54b2411
Aliane ISIMBI <117778528+AlianeIsimbi@users.noreply.github.com>
2025-03-27 10:12:15 +0200

    Update solution - CodeHub

--- 7-kyu/password-validator/password-validator.js
+++ 7-kyu/password-validator/password-validator.js
@@ -1,25 +1,3 @@
 function password(str) {
-  let capital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  let small = 'abcdefghijklmnopqrstuvwxyz'
-  let number = '01234556789'
-  let capitalCount = 0
-  let smallCount = 0
-  let numberCount = 0
-  let invalid = 0;
-  for(i = 0; i<str.length; i++){
-    if(capital.includes(str[i])){
-      capitalCount++;
-    }else if(small.includes(str[i])){
-      smallCount++;
-    }else if(number.includes(str[i])){
-      numberCount++;
-    }else{
-      invalid++;
-    }
-  }
-  if(str.length >= 8 && capitalCount > 0 && smallCount > 0 && numberCount > 0){
-    return true;
-  }else{
-    return false;
-  }
+    return str.length >= 8 && /[A-Z]/.test(str) && /[a-z]/.test(str) && /[0-9]/.test(str);
 }
