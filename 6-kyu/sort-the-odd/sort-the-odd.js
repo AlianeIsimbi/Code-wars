a3aef2b491fc577a28ec876f8e124c63e7eb3e7e
Aliane ISIMBI <117778528+AlianeIsimbi@users.noreply.github.com>
2025-03-27 10:36:48 +0200

    Add solution - CodeHub

---
+++ 6-kyu/sort-the-odd/sort-the-odd.js
@@ -0,0 +1,6 @@
+function sortArray(array) {
+    let odds = array.filter(num => num % 2 !== 0).sort((a, b) => a - b);
+    let oddIndex = 0;
+    return array.map(num => (num % 2 !== 0 ? odds[oddIndex++] : num));
+}
+​
