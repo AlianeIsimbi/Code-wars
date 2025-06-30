62af59e7293c8887ea58a7fb0cde437f29084fc4
Aliane ISIMBI <117778528+AlianeIsimbi@users.noreply.github.com>
2025-06-30 10:56:27 +0200

    Add solution - CodeHub

---
+++ 6-kyu/message-validator/message-validator.js
@@ -0,0 +1,24 @@
+function isAValidMessage(message) {
+  if (message === ""){
+    return true;
+  } 
+  let splitted = message.split(/(\d+)/);
+  splitted = splitted.filter(part => part !== "");
+  if (splitted.length % 2 !== 0){
+    return false;
+  } 
+  for (let i = 0; i < splitted.length; i += 2) {
+    let numberPart = splitted[i];
+    let textPart = splitted[i + 1];
+    if (!/^\d+$/.test(numberPart)){
+      return false;
+    }
+    if (!/^[a-zA-Z]+$/.test(textPart)){
+        return false;
+      }
+    if (Number(numberPart) !== textPart.length){
+      return false;
+    } 
+  }
+  return true;
+}
