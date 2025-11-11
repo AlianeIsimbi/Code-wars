f19ee2efcf62bd8bfb4a2790e441ad97f7dc7f26
Aliane ISIMBI <117778528+AlianeIsimbi@users.noreply.github.com>
2025-11-11 11:38:21 +0200

    Update solution - CodeHub

--- 8-kyu/sum-without-highest-and-lowest-number/sum-without-highest-and-lowest-number.java
+++ 8-kyu/sum-without-highest-and-lowest-number/sum-without-highest-and-lowest-number.java
@@ -3,23 +3,8 @@ public class Kata
 {
   public static int sum(int[] numbers)
   {
-    if(numbers == null || numbers.length <= 1){
-      return 0;
-    }
+    if(numbers == null || numbers.length <= 1) return 0;
     Arrays.sort(numbers);
-    
-    ArrayList<Integer> list = new ArrayList<>();
-    for(int n: numbers){
-      list.add(n);
-    }
-    list.remove(0);
-    list.remove(list.size() - 1);
-    
-    int sum = 0;
-    for(int i = 0; i<list.size(); i++){
-      sum += list.get(i);
-    }
-    
-    return sum;
+    return Arrays.stream(numbers).skip(1).limit(numbers.length-2).sum();
   }
 }
