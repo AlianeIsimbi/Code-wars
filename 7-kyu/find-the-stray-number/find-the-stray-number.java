82e2a921053327ad85e21bda489262151cc8a2a8
Aliane ISIMBI <117778528+AlianeIsimbi@users.noreply.github.com>
2025-11-25 11:44:02 +0200

    Update solution - CodeHub

--- 7-kyu/find-the-stray-number/find-the-stray-number.java
+++ 7-kyu/find-the-stray-number/find-the-stray-number.java
@@ -2,9 +2,6 @@ import java.util.*;
 class Solution {
   static int stray(int[] numbers) {
     Arrays.sort(numbers);
-      if(numbers[0] != numbers[1]){
-        return numbers[0];
-      }
-    return numbers[numbers.length - 1];
-  }
+    return (numbers[0] != numbers[1]) ? numbers[0] : numbers[numbers.length - 1];
+    }
 }
