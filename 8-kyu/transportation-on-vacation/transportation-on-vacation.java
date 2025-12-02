c99d0b08ba8343f8db22aa72dc806d0dea1fe602
Aliane ISIMBI <117778528+AlianeIsimbi@users.noreply.github.com>
2025-12-02 14:52:22 +0200

    Add solution - CodeHub

---
+++ 8-kyu/transportation-on-vacation/transportation-on-vacation.java
@@ -0,0 +1,15 @@
+public class Kata {
+  public static int rentalCarCost(int d) {
+    int cost = d * 40;
+​
+    if (d >= 7) {
+      cost -= 50;
+    } else if (d >= 3) {
+      cost -= 20;
+    }
+​
+    return cost;
+  }
+}
+​
+​
