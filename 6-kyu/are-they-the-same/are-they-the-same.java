8beab3b033c72bb5ac4f0e78d97d4590fb5c8157
Aliane ISIMBI <117778528+AlianeIsimbi@users.noreply.github.com>
2025-12-04 14:24:04 +0200

    Add solution - CodeHub

---
+++ 6-kyu/are-they-the-same/are-they-the-same.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class AreSame {
+  
+  public static boolean comp(int[] a, int[] b) {
+    if(a == null || b == null) return false;
+    if(a.length != b.length) return false;
+    
+    for(int i = 0; i<a.length; i++){
+      a[i] = a[i] * a[i];
+    }
+    Arrays.sort(a);
+    Arrays.sort(b);
+   return Arrays.equals(a, b);
+    
+    }
+}
