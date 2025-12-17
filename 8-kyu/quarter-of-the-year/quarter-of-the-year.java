9b17ffff0c4bd3c273f7343f88251dbb24d086a6
Aliane ISIMBI <117778528+AlianeIsimbi@users.noreply.github.com>
2025-12-17 14:42:02 +0200

    Add solution - CodeHub

---
+++ 8-kyu/quarter-of-the-year/quarter-of-the-year.java
@@ -0,0 +1,13 @@
+public class Kata {
+    public static int quarterOf(int month) {
+        if(1 <= month && month <= 3){
+          return 1;
+        }else if(4 <= month && month <= 6){
+          return 2;
+        }else if (7 <= month && month <= 9){
+          return 3;
+        }else{
+          return 4;
+        }
+    }
+}
